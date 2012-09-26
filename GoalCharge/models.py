7d19b1ed0a8dfc781448c4e699ea49ae6458486e
Chris Higgins <chris@chigs.me>
2012-09-26 13:17:48 -0500

    Added base for models. Need to make sure correct setup

---
+++ GoalCharge/models.py
@@ -0,0 +1,46 @@
+import datetime
+from flask import url_for
+from GoalCharge import db
+
+class User(db.Document):
+    email = db.EmailField(required=True)
+    password = db.StringField(required=True)
+    display_name = db.StringField(max_length=75)
+    register_date= db.DateTimeField(default=datetime.datetime.now, required=True)
+    charge = db.IntField()
+    is_admin = db.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.email
+
+class Goal(db.Document):
+    title = db.StringField(required=True, max_length=150)
+    user = db.ReferenceField(User, required=True)
+    charge = db.IntField()
+    original = db.ReferenceField(Goal)
+    status = db.StringField(required=True, default='not_started')
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    #date comparator
+    #charge comparator
+
+    def __unicode__(self):
+        return self.title
+
+class Comment(db.Document):
+    user = db.ReferenceField(User, required=True)
+    message = db.StringField(required=True, max_length=255)
+    goal = db.ReferenceField(Goal, required=True)
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+
+    def __unicode__(self):
+        return "%s Comment: %s" % (goal.title, message)
+
+class Milestone(db.Document):
+    goal = db.ReferenceField(Goal, required=True)
+    message = db.StringField(required=True)
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    finished_at = db.DateTimeField()
+    period = db.StringFiel()
+
+    def __unicode__(self):
+        return "%s Milestone: %s" % (goal.title, message)
